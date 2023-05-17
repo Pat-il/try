65ef879153b49e1682b2b8f8329952408c4156ef
Pat-il <90674200+Pat-il@users.noreply.github.com>
2023-05-17 18:34:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.get('/hello', function (req, res) {
     res.send(person);
 });
 
-var server = app.listen(1107nction () {
+var server = app.listen(1107, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://localhost:1107ost, port)
+    console.log("Example app listening at http://localhost:1107", host, port)
 });
 
   
